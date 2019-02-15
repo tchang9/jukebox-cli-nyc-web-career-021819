55056b8ee0b2ae560ff3734d7b522a56c2adf24d
tchang <tlchang9@gmail.com>
2019-02-15 01:07:19 -0500

    Wrote #jukebox

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,68 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+
+# puts "Enter your name:"
+# users_name = gets.chomp
+
+# puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts"- list : displays a list of songs you can play"
+  puts"- play : lets you choose a song to play"
+  puts"- exit : exits this program"
+end
+
+def list(songs)
+  solution = []
+  songs.each_with_index {|song, index|
+    solution.push((index + 1).to_s + ". " + song)
+  }
+  puts solution
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_song = gets.chomp
+
+  if songs.include?(user_song)
+    puts "Playing #{user_song} + 3"
+  elsif user_song.to_i != 0 and user_song.to_i <= songs.length
+    puts "Playing " + songs[user_song.to_i - 1]
+  else
+    puts "Invalid input, please try again"
+  end 
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  command = gets.chomp
+
+  while command != "exit"
+    case 
+    when command == "help"
+      help
+      puts "Please enter a command:"
+      command = gets.chomp
+    when command == "list"
+      puts list(songs)
+      puts "Please enter a command:"
+      command = gets.chomp
+    when command == "play"
+      play(songs)
+      puts "Please enter a command:"
+      command = gets.chomp
+    end
+  end
+  exit_jukebox
+end
